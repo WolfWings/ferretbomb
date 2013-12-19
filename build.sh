9e4ccd874cad9ed2578188618ebd4cebba597ff2
WolfWings <wolfwings@wolfwings.us>
2013-12-19 07:06:34 -0600

    Adding minifying build script for JavaScript (Closure Compiler) and CSS (CSSMinifier). TODO: Due to the small size, inline the javascript entirely?

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+curl -s \
+	-d compilation_level=ADVANCED_OPTIMIZATIONS \
+	-d output_info=compiled_code \
+	-d output_format=text \
+	--data-urlencode "js_code@scripts.js.src" \
+	http://closure-compiler.appspot.com/compile \
+	-o scripts.js
+
+curl -s \
+	-X POST
+	--data-urlencode "input@styles.css.src" \
+	http://cssminifier.com/raw
+	-o styles.css
