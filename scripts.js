f24da6abfe74bb8cd1590e8a6212f355fef6cef3
WolfWings <wolfwings@wolfwings.us>
2013-12-22 18:29:59 -0600

    Uglifying the javascript code: Single-depth arrays get function names optimized much better by Closure than neatly nested structures. :(

--- scripts.js
+++ scripts.js
@@ -2,7 +2,11 @@
 
 window["$"] = {
 
-streamonline: false
+API_URL: (function(prefix, suffix){ "use strict";
+	return "https://api.twitch.tv/kraken/" + prefix + "/ferretbomb" + suffix;
+})
+
+,streamonline: false
 ,updatestreamonline: (function(status){ "use strict";
 	$.streamonline = status;
 })
@@ -16,7 +20,7 @@ streamonline: false
 	var counter = 0;
 
 	var memoryleakcap = function() {
-		if (this.readyState && this.readyState !== "loaded" && this.readyState !== "complete") {
+		if (this.readyState && this.readyState !== 'loaded' && this.readyState !== 'complete') {
 			return;
 		}
 
@@ -29,7 +33,7 @@ streamonline: false
 	return function(url, callback) { "use strict";
 		var uniqueName = 'callback_json' + (++counter);
 
-		var script = $.tags.create('script');
+		var script = $.tags_create('script');
 		script.src = url + (url.toString().indexOf('?') === -1 ? '?' : '&') + 'callback=' + uniqueName;
 		script.async = true;
 
@@ -43,19 +47,19 @@ streamonline: false
 
 		script.onload = script.onreadystatechange = memoryleakcap;
 
-		$.tags.find.tagname('head')[0].appendChild(script);
+		$.tags_find_tagname('head')[0].appendChild(script);
 
 		return uniqueName;
 	};
 }())
 
 ,reoutline: (function(content, outline) { "use strict";
-	var headings = "";
+	var headings = '';
 	var depth = 0;
 	var lastAnchor = undefined;
-	for (var tag = $.tags.find.id(content).firstChild; tag; tag = tag.nextSibling) {
-		if (/^[aA]$/.test(tag.nodeName) && (tag.getAttribute("name").length > 1)) {
-			lastAnchor = tag.getAttribute("name");
+	for (var tag = $.tags_find_id(content).firstChild; tag; tag = tag.nextSibling) {
+		if (/^[aA]$/.test(tag.nodeName) && (tag.getAttribute('name').length > 1)) {
+			lastAnchor = tag.getAttribute('name');
 			continue;
 		}
 		var isheader = /^[Hh]([1-6])$/.exec(tag.nodeName);
@@ -64,134 +68,132 @@ streamonline: false
 		}
 		var newdepth = parseInt(isheader[1], 10);
 		if (depth === newdepth) {
-			headings += "</li><li>";
+			headings += '</li><li>';
 		} else {
-			headings += new Array(Math.abs(depth - newdepth) + 1).join((depth < newdepth) ? "<ol><li>" : "</li></ol></li><li>");
+			headings += new Array(Math.abs(depth - newdepth) + 1).join((depth < newdepth) ? '<ol><li>' : '</li></ol></li><li>');
 			depth = newdepth;
 		}
 		if (lastAnchor === undefined) {
 			headings += tag.innerHTML;
 		} else {
-			headings += "<a href=\"#" + lastAnchor + "\">" + tag.innerHTML + "</a>";
+			headings += '<a href="#' + lastAnchor + '">' + tag.innerHTML + '</a>';
 			lastAnchor = undefined;
 		}
 	}
-	headings += new Array(depth + 1).join("</li></ol>");
-	headings = headings.split("<ol");
+	headings += new Array(depth + 1).join('</li></ol>');
+	headings = headings.split('<ol');
 	for (var index = 1; index < headings.length; index++) {
-		if (headings[index].indexOf("</ol>") !== -1) {
-			headings[index] = " class=\"leaf\"" + headings[index];
+		if (headings[index].indexOf('</ol>') !== -1) {
+			headings[index] = ' class="leaf"' + headings[index];
 		}
 	}
-	headings = headings.join("<ol").split("<li");
+	headings = headings.join('<ol').split('<li');
 	for (var index = 1; index < headings.length; index++) {
-		if (headings[index].indexOf("<ol") === -1) {
-			headings[index] = " class=\"leaf\"" + headings[index];
+		if (headings[index].indexOf('<ol') === -1) {
+			headings[index] = ' class="leaf"' + headings[index];
 		}
 	}
-	$.tags.find.id(outline).innerHTML = headings.join("<li");
+	$.tags_find_id(outline).innerHTML = headings.join('<li');
 })
 
 ,updatebanner: (function() { "use strict";
-	$.JSONP("https://api.twitch.tv/kraken/channels/ferretbomb", function(response) {
+	$.JSONP($.API_URL('channels', ''), function(response) {
 		if ($.streampreview === undefined) {
-			$.updatestreampreview(response["video_banner"]);
+			$.updatestreampreview(response['video_banner']);
 		}
 	});
 })
 
 ,checkstream: (function() { "use strict";
-	$.JSONP("https://api.twitch.tv/kraken/streams/ferretbomb", function(response) {
-		if (response["stream"] === null) {
+	$.JSONP($.API_URL('streams', ''), function(response) {
+		if (response['stream'] === null) {
 			setTimeout($.updatebanner, 0);
 			setTimeout($.checkstream, 60000);
 			$.updatestreamonline(false);
 			return;
 		}
 		$.updatestreamonline(true);
-		$.updatestreampreview(response["stream"]["preview"]["large"]);
+		$.updatestreampreview(response['stream']['preview']['large']);
 		setTimeout($.checkstream, 600000);
 		return;
 	});
 })
 
-,classes: {
-	match: (function(theClass){ "use strict";
-		return new RegExp("(?:^|\\s)" + theClass + "(?!\\S)", "g");
-	})
-	,has: (function(tag, theClass) { "use strict";
-		return $.classes.match(theClass).test(tag["className"]);
-	})
-	,add: (function(tag, theClass) { "use strict";
-		tag["className"] += " " + theClass;
-		return tag;
-	})
-	,remove: (function(tag, theClass) { "use strict";
-		tag["className"] = tag["className"].replace($.classes.match(theClass), "");
-		return tag;
-	})
-	,toggle: (function(tag, theClass) { "use strict";
-		if ($.classes.has(tag, theClass)) {
-			$.classes.remove(tag, theClass);
-		} else {
-			$.classes.add(tag, theClass);
-		}
-		return tag;
-	})
-}
-
-,events: {
-	add: (function(object, event, callback){ "use strict";
-		if (object["attachEvent"]) {
-			object["attachEvent"]("on" + event, callback);
-		} else {
-			object["addEventListener"](event, callback, false);
-		}
-		return object;
-	})
-}
-
-,tags: {
-	create: (function(tagname){ "use strict";
-		return document["createElement"](tagname);
-	})
-	,find: {
-		tagname: (function(tagname){ "use strict";
-			return document["getElementsByTagName"](tagname);
-		})
-		,id: (function(id){ "use strict";
-			return document["getElementById"](id);
-		})
+,classes_match: (function(theClass){ "use strict";
+	return new RegExp('(?:^|\\s)' + theClass + '(?!\\S)', 'g');
+})
+,classes_has: (function(tag, theClass) { "use strict";
+	return $.classes_match(theClass).test(tag['className']);
+})
+,classes_add: (function(tag, theClass) { "use strict";
+	tag['className'] += ' ' + theClass;
+	return tag;
+})
+,classes_remove: (function(tag, theClass) { "use strict";
+	tag['className'] = tag['className'].replace($.classes_match(theClass), '');
+	return tag;
+})
+,classes_toggle: (function(tag, theClass) { "use strict";
+	if ($.classes_has(tag, theClass)) {
+		$.classes_remove(tag, theClass);
+	} else {
+		$.classes_add(tag, theClass);
 	}
-}
+	return tag;
+})
+
+,events_add: (function(object, event, callback){ "use strict";
+	if (object['attachEvent']) {
+		object['attachEvent']('on' + event, callback);
+	} else {
+		object['addEventListener'](event, callback, false);
+	}
+	return object;
+})
+
+,tags_create: (function(tagname){ "use strict";
+	return document['createElement'](tagname);
+})
+,tags_find_tagname: (function(tagname){ "use strict";
+	return document['getElementsByTagName'](tagname);
+})
+,tags_find_id: (function(id){ "use strict";
+	return document['getElementById'](id);
+})
+,tags_attribute_set: (function(tag, attribute, value){ "use strict";
+	tag.setAttribute(attribute, value);
+})
+,tags_append_child: (function(parent, child){ "use strict";
+	parent.appendChild(child);
+})
 
 ,"init": {
 	"article": (function() { "use strict";
-		var header = $.tags.find.tagname('header')[0];
-		var outline = $.tags.create('div');
-		outline.setAttribute('id', 'outline');
-		header.appendChild(outline);
-		var tail = $.tags.create('div');
-		tail.setAttribute('id', 'tail');
-		header.appendChild(tail);
+		var header = $.tags_find_tagname('header')[0];
+		var outline = $.tags_create('div');
+		$.tags_attribute_set(outline, 'id', 'outline');
+		$.tags_append_child(header, outline);
+		var tail = $.tags_create('div');
+		$.tags_attribute_set(tail, 'id', 'tail');
+		$.tags_append_child(header, tail);
 		var outlineToggleExpanded = (function(event){ "use strict";
 			var tag = event.toElement;
 			while (tag && !/^[Ll][Ii]$/.test(tag.tagName)) {
 				tag = tag.parentNode;
 			}
-			if ($.classes.has(tag, "leaf")) {
+			if ($.classes_has(tag, 'leaf')) {
 				return true;
 			}
-			$.classes.toggle(tag, "expanded");
+			$.classes_toggle(tag, 'expanded');
 			return true;
 		});
-		$.events.add(outline, "click", outlineToggleExpanded);
-		$.reoutline("content", "outline");
+		$.events_add(outline, 'click', outlineToggleExpanded);
+		$.reoutline('content', 'outline');
 	})
 
 	,"stream": (function() { "use strict";
-		var stream = $.tags.find.tagname('stream')[0];
-		var embed = $.tags.create('object');
+		var stream = $.tags_find_tagname('stream')[0];
+		var embed = $.tags_create('object');
 		var attribs = {
 			'type': 'application/x-shockwave-flash'
 			,'height': '100%'
@@ -204,19 +206,19 @@ streamonline: false
 			'allowFullScreen': 'true'
 			,'allowScriptAccess': 'true'
 			,'allowNetworking': 'all'
-			,'movie': 'http://www.twitch.tv/widgets/live_embed_player.sfw'
+			,'movie': 'http://www.twitch.tv/widgets/live_embed_player.swf'
 			,'flashvars': 'hostname=www.twitch.tv&channel=ferretbomb&auto_play=true&start_volume=100'
 		};
 		for (var attrib in attribs) {
-			embed.setAttribute(attrib, attribs[attrib]);
+			$.tags_attribute_set(embed, attrib, attribs[attrib]);
 		}
 		for (var param in params) {
-			var tag = $.tags.create('param');
-			tag.setAttribute('name', param);
-			tag.setAttribute('value', params[param]);
-			embed.appendChild(tag);
+			var tag = $.tags_create('param');
+			$.tags_attribute_set(tag, 'name', param);
+			$.tags_attribute_set(tag, 'value', params[param]);
+			$.tags_append_child(embed, tag);
 		}
-		stream.appendChild(embed);
+		$.tags_append_child(stream, embed);
 	})
 }
 
